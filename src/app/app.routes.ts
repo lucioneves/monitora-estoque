0bbc8b11a91ab5dda0573cb2c574a832ae61ae70
Lucio neves <luciocecilia20@gmail.com>
2024-06-27 14:21:51 -0400

    feat: adiciona roteamento das paginas login e home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/login/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent},
+   { path: 'home', component: HomeComponent},
+   { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' }
+];
