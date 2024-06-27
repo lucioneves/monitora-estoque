06f304174aca8f2b0b63d1ed1baafe0e840ae641
Lucio neves <luciocecilia20@gmail.com>
2024-06-27 18:52:38 -0400

    feat: adiciona authguard para as paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/login/home/home.component';
+import { LayoutComponent } from './feature/layout/layout.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent},
-   { path: 'home', component: HomeComponent},
-   { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { 
+        path: '', 
+        component: LayoutComponent,
+        canActivate: [AuthGuard],
+        children: [
+            { path: 'home', component: HomeComponent },
+            { path: 'news', component: HomeComponent }, // Adicione outras rotas necessárias
+            { path: 'contact', component: HomeComponent } // Adicione outras rotas necessárias
+        ]
+    },
     { path: '**', redirectTo: '/login' }
 ];
