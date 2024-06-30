f87292e6391a4626dc686d54949a225a31238903
Lucio neves <luciocecilia20@gmail.com>
2024-06-30 19:11:27 -0400

    feat: Adiciona as intarefaces ao projeto

---
+++ src/app/interface/payment.ts
@@ -0,0 +1,7 @@
+export interface Payment {
+    id?: number;
+    productName: string;
+    quantity: number;
+    deliveryDate: Date;
+    userName: string;
+}
---
+++ src/app/interface/product.ts
@@ -0,0 +1,7 @@
+export interface Product {
+    id?: number;
+    code: string;
+    description: string;
+    entryDate: Date;
+    expirationDate: Date;
+}
