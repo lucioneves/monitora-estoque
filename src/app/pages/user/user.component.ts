efcabb76ed00cb0dcd718714ee2646f2d9c04912
Lucio neves <luciocecilia20@gmail.com>
2024-06-30 19:15:04 -0400

    refactor: atualiza componente de cadastrar usuarios

--- src/app/pages/user/user.component.ts
+++ src/app/pages/user/user.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule} from '@angular/material/radio';
+import { UsersService } from '../../service/users.service';
 
-import { Router } from 'express';
 
 @Component({
   selector: 'app-user',
@@ -27,7 +27,39 @@ import { Router } from 'express';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent implements OnInit{
+
+
+  form: FormGroup;
+  hidePassword = true;
+
+  constructor(private fb: FormBuilder, private usersService: UsersService) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      // Lógica para enviar os dados para o serviço
+      this.usersService.addUser(this.form.value).subscribe(response => {
+        console.log('User added successfully', response);
+        this.form.reset();
+      });
+    }
+  }
+
+  togglePasswordVisibility(): void {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onClick(): void {
+    // Lógica para o botão "Salvar"
+    this.onSubmit();
+  }
   
 }
 
